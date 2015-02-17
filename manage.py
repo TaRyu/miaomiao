7a8b675f95794474e4c0e7d57855519ba148b886
TaRyu <taryu1991@gmail.com>
2015-02-18 03:14:38 +0800

    heroku部署

--- manage.py
+++ manage.py
@@ -15,5 +15,14 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
+@manager.command
+def profile(length=25, profile_dir=None):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+
 if __name__ == '__main__':
     manager.run()
